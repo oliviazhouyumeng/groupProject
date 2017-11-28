3823799506ca6bba69fa9b5e81d56ea8549acf56
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-11-27 20:44:23 -0500

    Add files via upload

---
+++ level.cc
@@ -0,0 +1,21 @@
+#include <memory>
+#include "level.h"
+#include "LevelImpl.h"
+#include "Block.h"
+#include "IBlock.h"
+#include "JBlock.h"
+#include "LBlock.h"
+#include "OBlock.h"
+#include "SBlock.h"
+#include "ZBlock.h"
+#include "TBlock.h"
+
+#include "StarBlock.h"
+
+Level::Level(int levelNum, bool isRandom): pimpl{make_unique<LevelImpl>(levelNum, isRandom)} {}
+
+unique_ptr<LevelImpl> &Level::getPImpl() {
+  return pimpl;
+}
+
+Level::~Level() = default;
