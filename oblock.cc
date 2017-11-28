8bf6951c97f1136577731a0dec4e8dcf6c24b130
picaone <33794897+picaone@users.noreply.github.com>
2017-11-27 21:16:54 -0500

    Create oblock.cc

---
+++ oblock.cc
@@ -0,0 +1,10 @@
+#include "abstractblock.h"
+#include "oblock.h"
+using namespace std;
+
+void OBlock::left();
+void OBlock::right();
+void OBlock::down();
+void OBlock::clockwise() {} // remains the same
+void OBlock::counterclockwise() {} // remains the same
+void OBlock::drop();
