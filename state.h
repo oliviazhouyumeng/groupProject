3a4a6d257c9b8166b1d7aa2a73a0f443d7ab7bc0
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 14:18:11 -0500

    Update state.h

--- state.h
+++ state.h
@@ -3,7 +3,7 @@
 
 enum class Colour {White=0, Black, Red, Green, Blue, Cyan, Yellow, Magenta, Orange, Brown};
 enum class Direction {N, W, E, S};
-enum class StateType {};
+enum class StateType {AC, NA}; // Active/NotActive
 
 struct State {
     Colour colour;
