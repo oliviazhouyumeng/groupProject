1963cc615ddf9ec89dff4be605bd5ca49adc19b8
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 13:54:40 -0500

    Update block.h

--- block.h
+++ block.h
@@ -8,7 +8,7 @@ protected:
     std::unique_ptr<Cell> lowerleft;
     std::string type; // type: "N"(default), "E", "S", "W"
     std::vector<std::unique_ptr<Cell>> cells; // a Block originally contains four Cell
-    bool heavy;
+    bool heavy; // default: false
 public:
     virtual void left() = 0;
     virtual void right() = 0;
