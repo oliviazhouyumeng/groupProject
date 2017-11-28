2b23648127e77e7495eee2333409727aa7a2f23b
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 17:03:21 -0500

    Update block.h

--- block.h
+++ block.h
@@ -5,9 +5,9 @@
 
 class Block {
 protected:
-    std::unique_ptr<Cell> lowerleft;
+    std::share_ptr<Cell> lowerleft;
     std::string type; // type: "A"(default), "B", "C", "D" clockwise:A->B->C->D
-    std::vector<std::unique_ptr<Cell>> cells; // a Block originally contains four Cell
+    std::vector<std::share_ptr<Cell>> cells; // a Block originally contains four Cell
     bool heavy; // default: false
 public:
     virtual void left() = 0;
