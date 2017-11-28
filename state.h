4a6a823dc960e518763e1c806d5d1ebd4e45d814
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 15:49:06 -0500

    Update state.h

--- state.h
+++ state.h
@@ -3,7 +3,7 @@
 
 enum class Colour {White=0, Black, Red, Green, Blue, Cyan, Yellow, Magenta, Orange, Brown};
 enum class Direction {N, W, E, S};
-enum class StateType {AC, NA}; // Active:default, can move&rotate
+enum class StateType {AC, NA}; // Active:default, can move&rotate, cells in the current block
                                // NotActive: change to notactive after drop, cannot move&rotate
 
 struct State {
