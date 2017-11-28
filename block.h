14c900e73b6c22cc01733d179c522e6a744f43f4
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 13:54:03 -0500

    Update block.h

--- block.h
+++ block.h
@@ -6,7 +6,7 @@
 class Block {
 protected:
     std::unique_ptr<Cell> lowerleft;
-    std::string type; // type: "N", "E", "S", "W"
+    std::string type; // type: "N"(default), "E", "S", "W"
     std::vector<std::unique_ptr<Cell>> cells; // a Block originally contains four Cell
     bool heavy;
 public:
