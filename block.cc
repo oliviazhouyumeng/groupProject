3f9f57b8c8edf253c9cbc660dcf7d8683ad35e8f
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 13:12:34 -0500

    Update block.cc

--- block.cc
+++ block.cc
@@ -1,11 +1,6 @@
 #include "block.h"
 using namespace std;
 
-
-unique_ptr<BlockImpl> Block::&getPImpl() {
-    return pImpl;
-}
-
-bool Block::isHeavy() const {
+bool Block::isHeavy() const{
     return heavy;
 }
