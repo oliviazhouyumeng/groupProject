947f981d22687260c4e0be52e7983dd665c2f90a
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-11-27 21:14:38 -0500

    Update blockimpl.h

--- blockimpl.h
+++ blockimpl.h
@@ -6,7 +6,7 @@
 struct BlockImpl {
     Cell *lowerleft;
     std::string type; // type: "N", "E", "S", "W"
-    std::vector<Cell*> cells; // a Block originally contains four Cell
+    std::vector<std::unique_ptr<Cell>> cells; // a Block originally contains four Cell
     bool heavy;
 };
 
