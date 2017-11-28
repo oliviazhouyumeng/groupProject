a29c177a81c40e1ee8c8ec02350b1beba1b233be
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 14:26:32 -0500

    add function to switch type when rotating

--- block.cc
+++ block.cc
@@ -1,6 +1,25 @@
 #include "block.h"
 using namespace std;
 
+Block::Block():lowerleft{nullptr}, type{"A"}, cells{vector<nullptr>}, heavy{false} {}
+
 bool Block::isHeavy() const{
     return heavy;
+}
+
+void Block::cwtype() {
+    switch(type) {
+        case("A") type = "B";
+        case("B") type = "C";
+        case("C") type = "D";
+        case("D") type = "A";
+    }
+}
+void Block::ccwtype() {
+    switch(type) {
+        case("A") type = "D";
+        case("B") type = "A";
+        case("C") type = "B";
+        case("D") type = "C";
+    }
 }
