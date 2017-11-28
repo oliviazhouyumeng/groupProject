c732224de76a58e005bdc6788d81854ddb51d3cb
picaone <33794897+picaone@users.noreply.github.com>
2017-11-27 21:10:56 -0500

    Create iblock.cc

---
+++ iblock.cc
@@ -0,0 +1,10 @@
+#include "abstractblock.h"
+#include "iblock.h"
+using namespace std;
+
+void IBlock::left();
+void IBlock::right();
+void IBlock::down();
+void IBlock::clockwise();
+void IBlock::counterclockwise();
+void IBlock::drop();
