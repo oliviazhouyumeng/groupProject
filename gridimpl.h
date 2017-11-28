95b7588350d1932abedda91d9f015b3710252841
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-11-27 21:12:04 -0500

    Update gridimpl.h

--- gridimpl.h
+++ gridimpl.h
@@ -1,21 +1,20 @@
-#ifndef gridimpl_h
-#define gridimpl_h
+#ifndef GRIDIMPL_H
+#define GRIDIMPL_H
 
 class TextDisplay;
 class GraphicsDisplay;
 
 struct GridImpl{
     std::vector<vector<cell>> theGrid; //the actual grid
-    std::vector<unique_ptr<Block>> liveBlocks;
-    unique_ptr<Level> l; //the current level
-    unique_ptr<Block> curr; // the current block on the board
-    unique_ptr<Block> next; // the next block will appear on the board
-    unique_ptr<TextDisplay> td = nullptr; // the text display
-    unique_ptr<GraphicsDisplay> gd = nullptr; // the graphics diaplay
+    std::vector<std::unique_ptr<Block>> liveBlocks;
+    std::unique_ptr<Level> l; //the current level
+    std::unique_ptr<Block> curr; // the current block on the board
+    std::unique_ptr<Block> next; // the next block will appear on the board
+    std::unique_ptr<TextDisplay> td = nullptr; // the text display
+    std::unique_ptr<GraphicsDisplay> gd = nullptr; // the graphics diaplay
     int hi_score; //the highest score in game
     int curr_score; // the current score in game
-    unique_ptr<Observer<Info, State>> ob = nullptr; //Another observer
+    std::unique_ptr<Observer<Info, State>> ob = nullptr; //Another observer
 };
-
 
 #endif
