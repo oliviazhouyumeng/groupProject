bd27297f7997adc00dd99c7c422bec3c7d1e0fd7
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-27 21:03:37 -0500

    Update grid.h

--- grid.h
+++ grid.h
@@ -3,28 +3,28 @@
 #include <iostream>
 #include <vector>
 #include <cstddef>
-#include "cell.h"
+#include "cell.hpp"
 #include "state.h"
 #include "info.h"
 #include "subject.h"
-#include "textdisplay.h"
-#include "graphicsdisplay.h"
-#include "level.h"
-
+#include "textdisplay.hpp"
+#include "graphicsdisplay.hpp"
+#include "level.hpp"
+#include <memory>
 class TextDisplay;
 class GraphicsDisplay;
 template <typename InfoType, typename StateType> class Observer;
 class InvalidMove{};
 class GridImpl;
 class Grid {
-    GridImpl *pImpl;
+    std::unique_ptr<GridImpl> pImpl;
     // Add private members, if necessary.
     
     
 public:
     Grid();
     ~Grid();
-    void setObserver(Observer<Info, State> *ob);
+    void setObserver(std::unique_ptr<Observer<Info, State>> ob);
     bool isReached() const;  // Is the game over
     void init(size_t r, size_t c, int win_size); // Sets up an n x n grid.  Clears old grid, if necessary.
     void setPiece(size_t r, size_t c, Colour colour);  // Plays piece at row r, col c.
