7751156860fd7cf4c5c23585b2c93a3f1d139e2a
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-28 16:27:56 -0500

    Update state.h

--- state.h
+++ state.h
@@ -1,7 +1,6 @@
 #ifndef state_h
 #define state_h
 
-enum class Colour {White=0, Black, Red, Green, Blue, Cyan, Yellow, Magenta, Orange, Brown};
 enum class Direction {N, W, E, S};
 enum class StateType {AC, NA}; // Active:default, can move&rotate, cells in the current block
                                // NotActive: change to notactive after drop, cannot move&rotate
