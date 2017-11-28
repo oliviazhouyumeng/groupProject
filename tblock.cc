70f9842ad5f153393b4d6c425bae4b89d58c578e
picaone <33794897+picaone@users.noreply.github.com>
2017-11-27 21:16:18 -0500

    Create tblock.cc

---
+++ tblock.cc
@@ -0,0 +1,10 @@
+#include "abstractblock.h"
+#include "tblock.h"
+using namespace std;
+
+void TBlock::left();
+void TBlock::right();
+void TBlock::down();
+void TBlock::clockwise();
+void TBlock::counterclockwise();
+void TBlock::drop();
