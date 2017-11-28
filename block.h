779abb49e25c98246537e21c8cbde1cc7bfb2ee0
picaone <33794897+picaone@users.noreply.github.com>
2017-11-28 17:30:48 -0500

    Update block.h

--- block.h
+++ block.h
@@ -5,7 +5,7 @@
 #include "cell.h"
 class Block {
 protected:
-    std::shared_ptr<Cell> lowerleft;
+    size_t x, y;
     std::string type; // type: "A"(default), "B", "C", "D" clockwise:A->B->C->D
     std::vector<std::shared_ptr<Cell>> cells; // a Block originally contains four Cell
     bool heavy; // default: false
@@ -18,7 +18,7 @@ public:
     virtual void drop() = 0;
     virtual void setNew(Grid &g) = 0; // set cells of the block
     bool isHeavy() const;
-    void cw(); // change type clockwise
-    void ccw(); // change type counterclockwise
+    void cwtype(); // change type clockwise
+    void ccwtype(); // change type counterclockwise
 };
 #endif
