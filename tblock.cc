94f0880b577533710e7481faac71fbdce095da12
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-28 23:35:33 -0500

    Update tblock.cc

--- tblock.cc
+++ tblock.cc
@@ -1,26 +1,91 @@
-#include "block.h"
-#include "tblock.h"
+#include "block.hpp"
+#include "tblock.hpp"
+
 using namespace std;
 
 
-TBlock::TBlock(): Block{3, 0, level, "A", false}{
-    auto a = std::make_shared<Cell>(2, 0, level);
+TBlock::TBlock(Grid &g): Block{3, 0, level, "A", false}{
+    shared_ptr<Cell> a(&g.getCell(2, 0));
     cells.push_back(a);
-    auto b = std::make_shared<Cell>(2, 1, level);
+    shared_ptr<Cell> b(&g.getCell(2, 1));
     cells.push_back(b);
-    auto c = std::make_shared<Cell>(2, 2, level);
+    shared_ptr<Cell> c(&g.getCell(2, 2));
     cells.push_back(c);
-    auto d = std::make_shared<Cell>(3, 1, level);
+    shared_ptr<Cell> d(&g.getCell(3, 1));
     cells.push_back(d);
 }
     
 
 
-void TBlock::left(){
+void TBlock::left(Grid &g){///////////not done
+    shared_ptr<Cell> ur(cells.at(2)); //upper right cell
+    int ur_x = ur->getInfo().row;
+    int ur_y = ur->getInfo().col;
+    //shared_ptr<Cell> new_ur(&g.getCell(size_t x, <#size_t y#>))
     
+    
+    
+    
+    
+    
+    shared_ptr<Cell> ur(cells.at(2)); //upper right cell
+    State news{StateType::ML};
+    ur->setState(news);
+    ur->notifyObservers();
 }
-void TBlock::right();
-void TBlock::down();
-void TBlock::clockwise();
-void TBlock::counterclockwise();
-void TBlock::drop();
+
+
+
+void TBlock::right(Grid &g){
+    shared_ptr<Cell> origin(&g.getCell(x, y));
+    origin->notifyObservers();
+}
+
+
+
+void TBlock::down(Grid &g){
+    shared_ptr<Cell> origin(&g.getCell(x, y));
+    origin->notifyObservers();
+}
+
+
+void TBlock::clockwise(Grid &g){
+    shared_ptr<Cell> origin(&g.getCell(x, y));
+    origin->notifyObservers();
+}
+void TBlock::counterclockwise(Grid &g){
+    shared_ptr<Cell> origin(&g.getCell(x, y));
+    origin->notifyObservers();
+}
+
+
+void TBlock::drop(Grid &g){
+    for(int i = 0; i < 18; i++){ //for 21 rows
+        down(g);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
