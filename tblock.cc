67fa94526da0758c64e9d966eafc2b78bfc08759
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-28 20:56:13 -0500

    Update tblock.cc

--- tblock.cc
+++ tblock.cc
@@ -2,7 +2,23 @@
 #include "tblock.h"
 using namespace std;
 
-void TBlock::left();
+
+TBlock::TBlock(): Block{3, 0, level, "A", false}{
+    auto a = std::make_shared<Cell>(2, 0, level);
+    cells.push_back(a);
+    auto b = std::make_shared<Cell>(2, 1, level);
+    cells.push_back(b);
+    auto c = std::make_shared<Cell>(2, 2, level);
+    cells.push_back(c);
+    auto d = std::make_shared<Cell>(3, 1, level);
+    cells.push_back(d);
+}
+    
+
+
+void TBlock::left(){
+    
+}
 void TBlock::right();
 void TBlock::down();
 void TBlock::clockwise();
