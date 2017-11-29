36d2fd03f0dfac8e500ec6447ae06203737307b6
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-28 20:55:20 -0500

    Update block.h

--- block.h
+++ block.h
@@ -4,21 +4,24 @@
 #include <vector>
 #include "cell.h"
 class Block {
- protected:
-  size_t x, y;
-  std::string rotateType; // type: "A"(default), "B", "C", "D" clockwise:A->B->C->D
-  std::vector<std::shared_ptr<Cell>> cells; // a Block originally contains four Cell
-  bool heavy; // default: false
- public:
-  virtual void left() = 0;
-  virtual void right() = 0;
-  virtual void down() = 0;
-  virtual void clockwise() = 0;
-  virtual void counterclockwise() = 0;
-  virtual void drop() = 0;
-  virtual void setNew(Grid &g) = 0; // set cells of the block
-  bool isHeavy() const;
-  void cwtype(); // change type clockwise
-  void ccwtype(); // change type counterclockwise
+protected:
+    size_t x, y;
+    int level;
+    std::string type; // type: "A"(default), "B", "C", "D" clockwise:A->B->C->D
+    bool heavy; // default: false
+    std::vector<std::shared_ptr<Cell>> cells; // a Block originally contains four Cell
+public:
+    Block(size_t x, size_t y, int level, std::string type, bool heavy);
+    virtual void left() = 0;
+    virtual void right() = 0;
+    virtual void down() = 0;
+    virtual void clockwise() = 0;
+    virtual void counterclockwise() = 0;
+    virtual void drop() = 0;
+    virtual void setNew(Grid &g) = 0; // set cells of the block
+    bool isHeavy() const;
+    void cwtype(); // change type clockwise
+    void ccwtype(); // change type counterclockwise
 };
 #endif
+
