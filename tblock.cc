c4c701eb5385e13828084c9f743204966a4b136b
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-30 17:32:14 -0500

    Update tblock.cc

--- tblock.cc
+++ tblock.cc
@@ -1,17 +1,22 @@
-#include "block.hpp"
-#include "tblock.hpp"
+#include "block.h"
+#include "tblock.h"
 
 using namespace std;
 
 
 TBlock::TBlock(Grid &g): Block{3, 0, level, "A", false}{
     auto a = make_shared<Cell>(&g.getCell(2, 0));
+    State newS = {StateType::AC};
+    a->setState(newS);
     cells.push_back(a);
     auto b = make_shared<Cell>(&g.getCell(2, 1));
+    b->setState(newS);
     cells.push_back(b);
     auto c = make_shared<Cell>(&g.getCell(2, 2));
+    c->setState(newS);
     cells.push_back(c);
     auto d = make_shared<Cell>(&g.getCell(3, 1));
+    d->setState(newS);
     cells.push_back(d);
 }
     
@@ -23,23 +28,149 @@ TBlock::TBlock(Grid &g): Block{3, 0, level, "A", false}{
 //std::vector<std::shared_ptr<Cell>> cells; // a Block originally contains four Cell
 
 void TBlock::left(Grid &g){///////////not done
+    State old_S = State{StateType::NA};
+    State new_S = State{StateType::AC};
     if (type == "A") {
-        <#statements#>
+        Cell &new_ul_ref = g.getCell(x-1, y-1);//upper left
+        Cell &new_lm_ref = g.getCell(x, y); //lower middle
+        if(new_ul_ref.getInfo().colour == Colour::White && new_lm_ref.getInfo().colour == Colour::White){
+            auto old_ur = make_shared<Cell>(cells.at(2));
+            auto old_lm = make_shared<Cell>(cells.at(3));
+            new_ul_ref.setColour(Colour::Orange, g);
+            new_lm_ref.setColour(Colour::Orange, g);
+            old_ur->setColour(Colour::White, g);
+            old_lm->setColour(Colour::White, g);
+            
+            cells.pop_back();
+            cells.pop_back();
+            auto new_ul = make_shared<Cell>(g.getCell(x-1, y-1));
+            auto new_lm = make_shared<Cell>(g.getCell(x, y));
+            cells.push_back(new_lm);
+            cells.insert(cells.begin(), new_ul);
+            old_ur->setState(old_S);
+            old_lm->setState(old_S);
+            new_ul->setState(new_S);
+            new_lm->setState(new_S);
+            old_ur->notifyObservers();
+            old_lm->notifyObservers();
+            new_ul->notifyObservers();
+            new_lm->notifyObservers();
+            y--;
+        }
     }
     else if (type == "B"){
-        
+        Cell &new_ur_ref = g.getCell(x-2, y);
+        Cell &new_ml_ref = g.getCell(x-1, y-1);
+        Cell &new_lr_ref = g.getCell(x, y);
+        if(new_ur_ref.getInfo().colour == Colour::White && new_ml_ref.getInfo().colour == Colour::White && new_lr_ref.getInfo().colour == Colour::White){
+            auto old_ur = make_shared<Cell>(cells.at(0));
+            auto old_mr = make_shared<Cell>(cells.at(1));
+            auto old_lr = make_shared<Cell>(cells.at(2));
+            new_ur_ref.setColour(Colour::Orange, g);
+            new_ml_ref.setColour(Colour::Orange, g);
+            new_lr_ref.setColour(Colour::Orange, g);
+            old_ur->setColour(Colour::White, g);
+            old_mr->setColour(Colour::White, g);
+            old_lr->setColour(Colour::White, g);
+            
+            cells.erase(cells.begin());
+            cells.erase(cells.begin());
+            cells.erase(cells.begin());
+            auto new_ur = make_shared<Cell>(g.getCell(x-2, y));
+            auto new_ml= make_shared<Cell>(g.getCell(x-1, y-1));
+            auto new_lr = make_shared<Cell>(g.getCell(x, y));
+            old_ur->setState(old_S);
+            old_mr->setState(old_S);
+            old_lr->setState(old_S);
+            new_ur->setState(new_S);
+            new_ml->setState(new_S);
+            new_lr->setState(new_S);
+            cells.insert(cells.begin(), new_ur);
+            cells.push_back(new_lr);
+            cells.push_back(new_ml);
+            old_ur->notifyObservers();
+            old_mr->notifyObservers();
+            old_lr->notifyObservers();
+            new_ur->notifyObservers();
+            new_ml->notifyObservers();
+            new_lr->notifyObservers();
+            y--;
+        }
     }
     else if(type == "C"){
-        
+        Cell &new_ll_ref = g.getCell(x, y-1);
+        Cell &new_um_ref = g.getCell(x-1, y);
+        if(new_ll_ref.getInfo().colour == Colour::White && new_um_ref.getInfo().colour == Colour::White){
+            auto old_lr = make_shared<Cell>(cells.at(0));
+            auto old_um = make_shared<Cell>(cells.at(3));
+            new_ll_ref.setColour(Colour::Orange, g);
+            new_um_ref.setColour(Colour::Orange, g);
+            old_lr->setColour(Colour::White, g);
+            old_um->setColour(Colour::White, g);
+            
+            cells.pop_back();
+            cells.erase(cells.begin());
+            auto new_ll = make_shared<Cell>(g.getCell(x, y-1));
+            auto new_um = make_shared<Cell>(g.getCell(x-1, y));
+            old_lr->setState(old_S);
+            old_um->setState(old_S);
+            new_ll->setState(new_S);
+            new_um->setState(new_S);
+            cells.push_back(new_ll);
+            cells.push_back(new_um);
+            old_lr->notifyObservers();
+            old_um->notifyObservers();
+            new_um->notifyObservers();
+            new_ll->notifyObservers();
+            y--;
+        }
     }
     else { // type "D"
-        
+        Cell &new_ul_ref = g.getCell(x-2, y-1);
+        Cell &new_ml_ref = g.getCell(x-1, y-1);
+        Cell &new_ll_ref = g.getCell(x, y-1);
+        if(new_ul_ref.getInfo().colour == Colour::White && new_ml_ref.getInfo().colour == Colour::White && new_ll_ref.getInfo().colour == Colour::White){
+            auto old_ul = make_shared<Cell>(cells.at(2));
+            auto old_mr = make_shared<Cell>(cells.at(3));
+            auto old_ll = make_shared<Cell>(cells.at(0));
+            new_ul_ref.setColour(Colour::Orange, g);
+            new_ml_ref.setColour(Colour::Orange, g);
+            new_ll_ref.setColour(Colour::Orange, g);
+            old_ul->setColour(Colour::White, g);
+            old_mr->setColour(Colour::White, g);
+            old_ll->setColour(Colour::White, g);
+            
+            cells.pop_back();
+            cells.pop_back();
+            cells.erase(cells.begin());
+            auto new_ul = make_shared<Cell>(g.getCell(x-2, y-1));
+            auto new_ml = make_shared<Cell>(g.getCell(x-1, y-1));
+            auto new_ll = make_shared<Cell>(g.getCell(x, y-1));
+            old_ul->setState(old_S);
+            old_mr->setState(old_S);
+            old_ll->setState(old_S);
+            new_ul->setState(new_S);
+            new_ml->setState(new_S);
+            new_ll->setState(new_S);
+            cells.insert(cells.begin(), new_ul);
+            cells.insert(cells.begin(), new_ml);
+            cells.insert(cells.begin(), new_ll);
+            old_ul->notifyObservers();
+            old_ll->notifyObservers();
+            old_mr->notifyObservers();
+            new_ul->notifyObservers();
+            new_ml->notifyObservers();
+            new_ll->notifyObservers();
+            y--;
+        }
     }
 }
 
 
 
 void TBlock::right(Grid &g){
+    State old_S = State{StateType::NA};
+    State new_S = State{StateType::AC};
     if (type == "A") {
         auto ur = make_shared<Cell>(cells.at(2)); //upper right cell
         size_t ur_x = ur->getInfo().row;
@@ -58,6 +189,10 @@ void TBlock::right(Grid &g){
             cells.erase(cells.begin()+2); // old forth element(lower middle)
             auto new_ur = make_shared<Cell>(&g.getCell(ur_x, ur_y+1));
             auto new_lm = make_shared<Cell>(&g.getCell(ur_x+1, ur_y));
+            old_ul->setState(old_S);
+            old_lm->setState(old_S);
+            new_ur->setState(new_S);
+            new_lm->setState(new_S);
             cells.push_back(new_ur);
             cells.push_back(new_lm);
             old_ul->notifyObservers();
@@ -92,6 +227,12 @@ void TBlock::right(Grid &g){
             auto new_ur = make_shared<Cell>(&g.getCell(mr_x-2, mr_y+2));
             auto new_mr = make_shared<Cell>(&g.getCell(mr_x-1, mr_y+2));
             auto new_lr = make_shared<Cell>(&g.getCell(mr_x, mr_y+2));
+            old_ml->setState(old_S);
+            old_ur->setState(old_S);
+            old_lr->setState(old_S);
+            new_ur->setState(new_S);
+            new_mr->setState(new_S);
+            new_lr->setState(new_S);
             cells.insert(cells.begin(), new_ur);
             cells.insert(cells.begin()+1, new_mr);
             cells.insert(cells.begin()+2, new_lr);
@@ -124,6 +265,10 @@ void TBlock::right(Grid &g){
             
             auto new_lr = make_shared<Cell>(&g.getCell(lr_x, lr_y+1));
             auto new_um = make_shared<Cell>(&g.getCell(lr_x-1, lr_y));
+            old_ll->setState(old_S);
+            old_um->setState(old_S);
+            new_lr->setState(new_S);
+            new_um->setState(new_S);
             cells.insert(cells.begin(), new_lr);
             cells.push_back(new_um);
             old_um->notifyObservers();
@@ -158,6 +303,12 @@ void TBlock::right(Grid &g){
             auto new_ul = make_shared<Cell>(&g.getCell(mr_x-1, mr_y));
             auto new_mr = make_shared<Cell>(&g.getCell(mr_x, mr_y+1));
             auto new_ll = make_shared<Cell>(&g.getCell(mr_x+1, mr_y));
+            old_ul->setState(old_S);
+            old_ml->setState(old_S);
+            old_ll->setState(old_S);
+            new_ul->setState(new_S);
+            new_mr->setState(new_S);
+            new_ll->setState(new_S);
             cells.insert(cells.begin(), new_ll);
             cells.push_back(new_ul);
             cells.push_back(new_mr);
@@ -175,12 +326,62 @@ void TBlock::right(Grid &g){
 
 
 void TBlock::down(Grid &g){
-    shared_ptr<Cell> origin(&g.getCell(x, y));
-    origin->notifyObservers();
+    auto old_ul = make_shared<Cell>(cells.at(0));
+    auto old_um = make_shared<Cell>(cells.at(1));
+    auto old_ur = make_shared<Cell>(cells.at(2));
+    auto old_lm = make_shared<Cell>(cells.at(3));
+    auto new_ul = make_shared<Cell>(&g.getCell(old_ul->getInfo().row + 1, old_ul->getInfo().col));
+    auto new_um = make_shared<Cell>(&g.getCell(old_um->getInfo().row + 1, old_um->getInfo().col));
+    auto new_ur = make_shared<Cell>(&g.getCell(old_ur->getInfo().row + 1, old_ur->getInfo().col));
+    auto new_lm = make_shared<Cell>(&g.getCell(old_lm->getInfo().row + 1, old_lm->getInfo().col));
+    if((new_ul->getInfo().colour == Colour::White || new_ul->getState().stype == StateType::AC) &&
+       (new_um->getInfo().colour == Colour::White || new_um->getState().stype == StateType::AC) &&
+       (new_ur->getInfo().colour == Colour::White || new_ur->getState().stype == StateType::AC) &&
+       (new_lm->getInfo().colour == Colour::White || new_lm->getState().stype == StateType::AC)){
+        old_ul->setColour(Colour::White, g);
+        old_um->setColour(Colour::White, g);
+        old_ur->setColour(Colour::White, g);
+        old_lm->setColour(Colour::White, g);
+        State old_S = State{StateType::NA};
+        old_ul->setState(old_S);
+        old_um->setState(old_S);
+        old_ur->setState(old_S);
+        old_lm->setState(old_S);
+        new_ul->setColour(Colour::Orange, g);
+        new_um->setColour(Colour::Orange, g);
+        new_ur->setColour(Colour::Orange, g);
+        new_lm->setColour(Colour::Orange, g);
+        State new_S = State{StateType::AC};
+        new_ul->setState(new_S);
+        new_um->setState(new_S);
+        new_ur->setState(new_S);
+        new_lm->setState(new_S);
+        cells.clear();
+        cells.push_back(new_ul);
+        cells.push_back(new_um);
+        cells.push_back(new_ur);
+        cells.push_back(new_lm);
+        old_ul->notifyObservers();
+        old_um->notifyObservers();
+        old_ur->notifyObservers();
+        old_lm->notifyObservers();
+        new_ul->notifyObservers();
+        new_um->notifyObservers();
+        new_ur->notifyObservers();
+        new_lm->notifyObservers();
+        x++;
+    }
 }
 
 
 void TBlock::clockwise(Grid &g){
+    
+    
+    
+    
+    
+    
+    
     shared_ptr<Cell> origin(&g.getCell(x, y));
     origin->notifyObservers();
 }
