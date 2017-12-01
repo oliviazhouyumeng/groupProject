ce0cff142d941fea738e6f6b025ef61f42d04868
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 11:38:23 -0500

    Update sblock.h

--- sblock.h
+++ sblock.h
@@ -1,15 +1,17 @@
 #ifndef SBLOCK_H
 #define SBLOCK_H
 #include "block.h"
+#include "grid.h"
 
 class SBlock : public Block {
 public:
-    void left() override;
-    void right() override;
-    void down() override;
-    void clockwise() override;
-    void counterclockwise() override;
-    void drop() override;
+    SBlock(Grid &g, int level);
+    void left(Grid &g) override;
+    void right(Grid &g) override;
+    void down(Grid &g) override;
+    void clockwise(Grid &g) override;
+    void counterclockwise(Grid &g) override;
+    void drop(Grid &g) override;
 };
 
 #endif
