870e3ecd4f5700d7b9146c5352e2f27b9cf64c62
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-01 01:02:54 -0500

    Update iblock.h

--- iblock.h
+++ iblock.h
@@ -4,7 +4,7 @@
 
 class IBlock : public Block {
 public:
-    IBlock(int level, bool heavy = false);
+    IBlock(Grid &g, int level);
     void left() override;
     void right() override;
     void down() override;
