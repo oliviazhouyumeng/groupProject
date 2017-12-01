8f38582031f262ef56b540631e4a82d38daf760d
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-11-30 23:44:43 -0500

    Update block.h

--- block.h
+++ block.h
@@ -3,23 +3,21 @@
 #include <string>
 #include <vector>
 #include "cell.h"
-
+#include "posn.h"
 class Block {
 protected:
-    size_t x, y;
     int level;
-    std::string type = "A"; // type: "A"(default), "B", "C", "D" clockwise:A->B->C->D
-    bool heavy = false; // default: false
-    std::vector<std::shared_ptr<Cell>> cells(4); // a Block originally contains four Cell
+    std::string type; // type: "A"(default), "B", "C", "D" clockwise:A->B->C->D
+    bool heavy; // default: false
+    std::vector<Posn> pos; // a Block originally contains four Cell
 public:
-    Block(size_t x, size_t y, int level, std::string type, bool heavy);
+    Block(int level, std::string type, bool heavy);
     virtual void left(Grid &g) = 0;
     virtual void right(Grid &g) = 0;
     virtual void down(Grid &g) = 0;
     virtual void clockwise(Grid &g) = 0;
     virtual void counterclockwise(Grid &g) = 0;
     virtual void drop(Grid &g) = 0;
-    virtual void setNew(Grid &g) = 0; // set cells of the block
     bool isHeavy() const;
     void cwtype(); // change type clockwise
     void ccwtype(); // change type counterclockwise
