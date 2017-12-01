b385358d7f16c5207077b793d1d12d16096d1531
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-11-30 23:42:14 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -6,14 +6,14 @@
 #include "state.h"
 #include "block.h"
 #include "level.h"
-#include "sstream"
+#include <sstream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cin.exceptions(ios::eofbit|ios::failbit);
     bool textMode = false;
     int startLevel = 0;
-    for (int i = 1; i < argc;){
+    for (int i = 1; i < argc; ++i){
         string curArg = argv[i];
         if (curArg == "-text") {
             textMode = true;
@@ -22,23 +22,25 @@ int main(int argc, char *argv[]) {
             int seedNum = argv[i];
             // sets the random number generator’s seed to xxx(seedNum)
         } else if (curArg == "scriptfile") {
-            ++i;
-            string scriptFile = argv[i];
+            if (i + 1 < argc && argv[i + 1] != "-seed" && argv[i + 1] != "-text" && 
+                argv[i + 1] != "startlevel" && argv[i + 1] != "scriptfile") {
+                ++i;
+                string scriptFile = argv[i];
+            }
             // Use scriptFile instead of sequence.txt as a source of blocks for level 0
         } else if (curArg == "startlevel") {
             ++i;
             startLevel = argv[i];
             // Starts the game in level n
         }
-        ++i;
     }
     
+    Grid g;
     string cmd;
-    if (textMode) Grid g; // text only grid
-    else Grid g; // text & graphic
-    g.init();
-    g.setLevel(startLevel);
-    //generate new blocks
+    if (textMode)
+        g.setGraphics(false); // init a grid w/ graphics disabled
+    g.init();  // Fill in parameters!
+    g.setLevel(startLevel); //generate new blocks
 
     try {
         while (true) {
