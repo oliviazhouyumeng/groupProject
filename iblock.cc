2aff0a5da34a98e4f4e3213675ef91866ab2ef9f
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-01 01:03:09 -0500

    Update iblock.cc

--- iblock.cc
+++ iblock.cc
@@ -2,6 +2,21 @@
 #include "iblock.h"
 using namespace std;
 
+const string startType = "A";
+const bool isHeavy = false;
+
+IBlock::Iblock(Grid &g, int level): Block{level, startType, isHeavy} {
+  if (level > 2) heavy = true;
+  Posn p1{3, 0};
+  Posn p2{3, 1};
+  Posn p3{3, 2};
+  Posn p4{3, 3};
+  pos.emplace_back(p1);
+  pos.emplace_back(p2);
+  pos.emplace_back(p3);
+  pos.emplace_back(p4);
+}
+  
 void IBlock::left();
 void IBlock::right();
 void IBlock::down();
