fbe778a0e730c682c52cae76ed54a689152e9e33
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 23:45:12 -0500

    Update zblock.cc

--- zblock.cc
+++ zblock.cc
@@ -9,17 +9,17 @@ ZBlock(Grid &g, int level): Block{level, "A", false} {
     Posn p1{3, 1};
     Posn p2{4, 1};
     Posn p3{4, 2};
-    for (auto p : pos) {
-        if (!g.checkWhite(p.x, p.y)) {g.endGame();}
-    }
-    g.setColour(3, 0, Colour::Magenta);
-    g.setColour(3, 1, Colour::Magenta);
-    g.setColour(4, 1, Colour::Magenta);
-    g.setColour(4, 2, Colour::Magenta);
     pos.emplace_back(p0);
     pos.emplace_back(p1);
     pos.emplace_back(p2);
     pos.emplace_back(p3);
+    for (auto p : pos) {
+        if (!g.checkWhite(p.x, p.y)) {g.endGame();}
+    }
+    for (auto p : pos) {
+        g.getColour(p.x, p.y, Colour::Magenta);
+        g.setCellLevel(p.x, p.y, level);
+    }
 }
 
 void ZBlock::left(Grid &g) {
@@ -27,9 +27,15 @@ void ZBlock::left(Grid &g) {
         if (p.y == 0) return;
         if ((!g.checkWhite(p.x, p.y-1))&&g.getCell(p.x, p.y-1).getState().stype==StateType::NA) return;
     }
-    for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+    for (auto p : pos) {
+        g.setColour(p.x, p.y, Colour::White);
+        g.setCellLevel(p.x, p.y, -1);
+    }
     for (auto p : pos) p.y--;
-    for (auto p : pos) g.setColour(p.x, p.y, Colour::Magenta);
+    for (auto p : pos) {
+        g.setColour(p.x, p.y, Colour::Magenta);
+        g.setCellLevel(p.x, p.y, level);
+    }
     if (heavy) down(g);
 }
 
@@ -38,9 +44,15 @@ void ZBlock::right(Grid &g) {
         if (p.y == 0) return;
         if ((!g.checkWhite(p.x, p.y+1))&&g.getCell(p.x, p.y+1).getState().stype==StateType::NA) return;
     }
-    for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+    for (auto p : pos) {
+        g.setColour(p.x, p.y, Colour::White);
+        g.setCellLevel(p.x, p.y, -1);
+    }
     for (auto p : pos) p.y++;
-    for (auto p : pos) g.setColour(p.x, p.y, Colour::Magenta);
+    for (auto p : pos) {
+        g.setColour(p.x, p.y, Colour::Magenta);
+        g.setCellLevel(p.x, p.y, level);
+    }
     if (heavy) down(g);
 }
 
@@ -49,35 +61,53 @@ void ZBlock::down(Grid &g) {
         if (p.x == 17) return;
         if (g.getCell(p.x+1, p.y).getInfo().colour != Colour::White) return;
     }
-    for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+    for (auto p : pos) {
+        g.setColour(p.x, p.y, Colour::White);
+        g.setCellLevel(p.x, p.y, -1);
+    }
     for (auto p : pos) p.x++;
-    for (auto p : pos) g.setColour(p.x, p.y, Colour::Magenta);
+    for (auto p : pos) {
+        g.setColour(p.x, p.y, Colour::Magenta);
+        g.setCellLevel(p.x, p.y, level);
+    }
 }
 
 void ZBlock::clockwise(Grid &g) {
     if (type == "A" || type == "C") {
         if (g.checkWhite(pos[0].x+1, pos[0].y) && g.checkWhite(pos[1].x-1, pos[1].y)) {
             cwtype();
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::White);
+                g.setCellLevel(p.x, p.y, -1);
+            }
             pos[0].x--;
             pos[0].y++;
             pos[2].x++;
             pos[2].y--;
             pos[3].y-=2;
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::Magenta);
+                g.setCellLevel(p.x, p.y, level);
+            }
             if (heavy) down(g);
         }
     } else if (type == "B" || type == "D") {
         if (g.checkWhite(pos[3].x, pos[3].y+1) && g.checkWhite(pos[3].x, pos[3].y+2)
             && pos[1].y!=10) {
             cwtype();
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::White);
+                g.setCellLevel(p.x, p.y, -1);
+            }
             pos[0].x++;
             pos[0].y--;
             pos[2].x--;
             pos[2].y++;
             pos[3].y+=2;
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::Magenta);
+                g.setCellLevel(p.x, p.y, level);
+            }
             if (heavy) down(g);
         }
     }
@@ -87,26 +117,38 @@ void ZBlock::counterclockwise(Grid &g) {
     if (type == "A" || type == "C") {
         if (g.checkWhite(pos[0].x+1, pos[0].y) && g.checkWhite(pos[1].x-1, pos[1].y)) {
             ccwtype();
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::White);
+                g.setCellLevel(p.x, p.y, -1);
+            }
             pos[0].x--;
             pos[0].y++;
             pos[2].x++;
             pos[2].y--;
             pos[3].y-=2;
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::Magenta);
+                g.setCellLevel(p.x, p.y, level);
+            }
             if (heavy) down(g);
         }
     } else if (type == "B" || type == "D") {
         if (g.checkWhite(pos[3].x, pos[3].y+1) && g.checkWhite(pos[3].x, pos[3].y+2)
             && pos[1].y!=10) {
             ccwtype();
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::White);
+                g.setCellLevel(p.x, p.y, -1);
+            }
             pos[0].x++;
             pos[0].y--;
             pos[2].x--;
             pos[2].y++;
             pos[3].y+=2;
-            for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
+            for (auto p : pos) {
+                g.setColour(p.x, p.y, Colour::Magenta);
+                g.setCellLevel(p.x, p.y, level);
+            }
             if (heavy) down(g);
         }
     }
