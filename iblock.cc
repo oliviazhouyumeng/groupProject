60a58d1358ce5eccd810de71f30515fe293a7391
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-01 23:43:50 -0500

    Update iblock.cc

--- iblock.cc
+++ iblock.cc
@@ -17,9 +17,44 @@ IBlock::Iblock(Grid &g, int level): Block{level, startType, isHeavy} {
   pos.emplace_back(p4);
 }
   
-void IBlock::left();
-void IBlock::right();
-void IBlock::down();
-void IBlock::clockwise();
+void IBlock::left() {
+  for (auto p : pos) {
+    if (p.y == 0) return;
+  }
+  for (auto p : pos) {
+    
+    p.y -= 1;
+    
+    // change colour
+  }
+}
+  
+void IBlock::right() {
+  for (auto p : pos) {
+    if (p.y == 0) return;
+  }
+  for (auto p : pos) {
+    p.y += 1;  // change colour
+  }
+}
+
+void IBlock::down() {
+  for (auto p : pos) {
+    if (p.y == 0) return;
+  }
+  for (auto p : pos) {
+    p.x += 1;  // change colour
+  }
+}
+
+void IBlock::clockwise() {
+  if (type == "A") {
+    for (auto p : pos) {
+    }
+    type = "B";
+  } else if (type == "B") {
+    for (auto p : pos) {
+      
+      
 void IBlock::counterclockwise();
 void IBlock::drop();
