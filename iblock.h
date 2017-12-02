b4f353687267090babbbe706b1f63fd1c34a431b
picaone <33794897+picaone@users.noreply.github.com>
2017-12-02 02:16:37 -0500

    Update iblock.h

--- iblock.h
+++ iblock.h
@@ -1,18 +1,15 @@
 #ifndef IBLOCK_H
 #define IBLOCK_H
-#include "block.hpp"
-#include "grid.hpp"
+#include "block.h"
 
 class IBlock : public Block {
 public:
-    IBlock(Grid &g, int level);
     void left(Grid &g) override;
     void right(Grid &g) override;
     void down(Grid &g) override;
     void clockwise(Grid &g) override;
     void counterclockwise(Grid &g) override;
     void drop(Grid &g) override;
-    void giveHint(Grid &g) override;
 };
 
 #endif
