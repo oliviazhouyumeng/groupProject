31e91ce9febab8e7b58e8ebe281ea1c8724e0ac9
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 19:20:18 -0500

    Update state.h

--- state.h
+++ state.h
@@ -1,12 +1,10 @@
 #ifndef state_h
 #define state_h
 
-enum class Direction {N, W, E, S};
 enum class StateType {AC, NA}; // Active:default, can move&rotate, cells in the current block
                                // NotActive: change to notactive after drop, cannot move&rotate
 
 struct State {
-    Direction direction;
     StateType stype;
 };
 
