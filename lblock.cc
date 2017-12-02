2fdf6b45a40d6347ad98e9156a1e3724ff426454
picaone <33794897+picaone@users.noreply.github.com>
2017-12-02 03:12:34 -0500

    Update lblock.cc

--- lblock.cc
+++ lblock.cc
@@ -6,7 +6,7 @@
 #include <vector>
 using namespace std;
 
-LBlock(Grid &g, int level): Block{level, "A", false} {
+LBlock::LBlock(Grid &g, int level): Block{level, "A", false} {
     if (level > 2) {heavy = true;}
     Posn p0{3, 2};
     Posn p1{4, 2};
