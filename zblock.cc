30c708e58bf12656200030af896370eb1dc89e88
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 20:14:04 -0500

    Update zblock.cc

--- zblock.cc
+++ zblock.cc
@@ -5,10 +5,10 @@ using namespace std;
 
 ZBlock(Grid &g, int level): Block{level, "A", false} {
     if (level > 2) {heavy = true;}
-    Posn p1{3, 0};
-    Posn p2{3, 1};
-    Posn p3{4, 1};
-    Posn p4{4, 2};
+    Posn p0{3, 0};
+    Posn p1{3, 1};
+    Posn p2{4, 1};
+    Posn p3{4, 2};
     for (auto p : pos) {
         if (!g.checkWhite(p.x, p.y)) {g.endGame();}
     }
@@ -16,10 +16,10 @@ ZBlock(Grid &g, int level): Block{level, "A", false} {
     g.setColour(3, 1, Colour::Magenta);
     g.setColour(4, 1, Colour::Magenta);
     g.setColour(4, 2, Colour::Magenta);
+    pos.emplace_back(p0);
     pos.emplace_back(p1);
     pos.emplace_back(p2);
     pos.emplace_back(p3);
-    pos.emplace_back(p4);
 }
 
 void ZBlock::left(Grid &g) {
@@ -56,27 +56,27 @@ void ZBlock::down(Grid &g) {
 
 void ZBlock::clockwise(Grid &g) {
     if (type == "A" || type == "C") {
-        if (g.checkWhite(pos[1].x+1, pos[2].y) && g.checkWhite(pos[2].x-1, pos[3].y)) {
+        if (g.checkWhite(pos[0].x+1, pos[0].y) && g.checkWhite(pos[1].x-1, pos[1].y)) {
             cwtype();
             for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
-            pos[1].x--;
-            pos[1].y++;
-            pos[3].x++;
-            pos[3].y--;
-            pos[4].y-=2;
+            pos[0].x--;
+            pos[0].y++;
+            pos[2].x++;
+            pos[2].y--;
+            pos[3].y-=2;
             for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
             if (heavy) down(g);
         }
     } else if (type == "B" || type == "D") {
-        if (g.checkWhite(pos[4].x, pos[4].y+1) && g.checkWhite(pos[4].x, pos[4].y+2)
-            && pos[2].y!=10) {
+        if (g.checkWhite(pos[3].x, pos[3].y+1) && g.checkWhite(pos[3].x, pos[3].y+2)
+            && pos[1].y!=10) {
             cwtype();
             for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
-            pos[1].x++;
-            pos[1].y--;
-            pos[3].x--;
-            pos[3].y++;
-            pos[4].y+=2;
+            pos[0].x++;
+            pos[0].y--;
+            pos[2].x--;
+            pos[2].y++;
+            pos[3].y+=2;
             for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
             if (heavy) down(g);
         }
@@ -85,27 +85,27 @@ void ZBlock::clockwise(Grid &g) {
 
 void ZBlock::counterclockwise(Grid &g) {
     if (type == "A" || type == "C") {
-        if (g.checkWhite(pos[1].x+1, pos[2].y) && g.checkWhite(pos[2].x-1, pos[3].y)) {
+        if (g.checkWhite(pos[0].x+1, pos[0].y) && g.checkWhite(pos[1].x-1, pos[1].y)) {
             ccwtype();
             for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
-            pos[1].x--;
-            pos[1].y++;
-            pos[3].x++;
-            pos[3].y--;
-            pos[4].y-=2;
+            pos[0].x--;
+            pos[0].y++;
+            pos[2].x++;
+            pos[2].y--;
+            pos[3].y-=2;
             for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
             if (heavy) down(g);
         }
     } else if (type == "B" || type == "D") {
-        if (g.checkWhite(pos[4].x, pos[4].y+1) && g.checkWhite(pos[4].x, pos[4].y+2)
-            && pos[2].y!=10) {
+        if (g.checkWhite(pos[3].x, pos[3].y+1) && g.checkWhite(pos[3].x, pos[3].y+2)
+            && pos[1].y!=10) {
             ccwtype();
             for (auto p : pos) g.setColour(p.x, p.y, Colour::White);
-            pos[1].x++;
-            pos[1].y--;
-            pos[3].x--;
-            pos[3].y++;
-            pos[4].y+=2;
+            pos[0].x++;
+            pos[0].y--;
+            pos[2].x--;
+            pos[2].y++;
+            pos[3].y+=2;
             for (auto p : pos) g.setColour(p.x, p.y, Colour::Yellow);
             if (heavy) down(g);
         }
@@ -113,7 +113,7 @@ void ZBlock::counterclockwise(Grid &g) {
 }
 
 void ZBlock::drop(Grid &g) {
-    size_t start = pos[2].x+1;
+    size_t start = pos[1].x+1;
     for (size_t i = start; i < 18; i++) {
         down(g);
     }
