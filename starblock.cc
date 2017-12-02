bb80ff11ac93732bd4fbe654a2d11f8527484ce2
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-12-01 22:01:19 -0500

    Update starblock.cc

--- starblock.cc
+++ starblock.cc
@@ -2,9 +2,42 @@
 #include "starblock.h"
 using namespace std;
 
-void StarBlock::left() {} // cannot move to left
-void StarBlock::right() {} // cannot move to right
-void StarBlock::down();
-void StarBlock::clockwise() {} // remains the same
-void StarBlock::counterclockwise() {} // remains the same
-void StarBlock::drop();
+const string startType = "A";
+const bool is_Heavy = false;
+
+StarBlock::StarBlock(Grid &g, int level):Block{level, startType, is_Heavy}{
+    if(level>2) {heavy = true;}
+    Posn single = Posn{3, 5};
+    if(!g.checkWhite(single.x, single.y)) {
+        g.endGame();
+    }
+    g.setColour(single.x, single.y, Colour::Brown);
+    pos.push_back(single);
+}
+
+void StarBlock::left(Grid &g) {} // cannot move to left
+void StarBlock::right(Grid &g) {} // cannot move to right
+void StarBlock::down(Grid &g){
+    for(auto p : pos) {
+        if(p.x == 17) return;
+        if(!g.checkWhite(p.x+1, p.y) && g.getCell(p.x+1, p.y).getState().stype == StateType::NA) return;
+    }
+    for(auto p : pos) g.setColour(p.x, p.y, Colour::White);
+    for(auto p : pos) p.x++;
+    for(auto p : pos) g.setColour(p.x, p.y, Colour::Brown);
+}
+void StarBlock::clockwise(Grid &g) {} // remains the same
+void StarBlock::counterclockwise(Grid &g) {} // remains the same
+void StarBlock::drop(Grid &g){
+    for (size_t i = pos.at(0).x; i<18; i++) {
+        down(g);
+    }
+    State s = State{StateType::NA};
+    for(auto p : pos){
+        g.gSetState(p.x, p.y, s);
+    }
+    for(auto p : pos){
+        if(g.isFull(p.x)) g.clearRow(p.x);
+    }
+    g.moveDown();
+}
