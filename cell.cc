7f4900e5a2945b982bf4433441fff6bfad3615ab
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 20:44:58 -0500

    Update cell.cc

--- cell.cc
+++ cell.cc
@@ -31,11 +31,7 @@ void Cell::setColour(Colour colour, Grid &g){ // Modifies colour of the cell
 void Cell::addScore(Grid &g, int score) { //modify the score field in Grid
     g.updateScore(score);
 }
-void Cell::notify(Subject<Info, State> &whoFrom, Grid &g) {
-    if(getInfo().colour == Colour::White) return;
-    if(getState().stype == StateType::AC) return;
-    
-}
+
 Info Cell::getInfo() const {
     Info rt{r, c, colour};
     return rt;
