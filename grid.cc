8f08da0c4d8588394223b0fc0847408a023d10e0
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 23:08:31 -0500

    Update grid.cc

--- grid.cc
+++ grid.cc
@@ -1,217 +1,75 @@
-#include "grid.h"
-#include "info.h"
-#include "state.h"
-#include "endexception.h"
-#include "block.h"
-#include "iblock.h"
-#include "jblock.h"
-#include "lblock.h"
-#include "sblock.h"
-#include "zblock.h"
-#include "oblock.h"
-#include "tblock.h"
-#include "hintblock.h"
-#include "starblock.h"
+#ifndef GRID_H
+#define GRID_H
+#include <iostream>
 #include <vector>
-using namespace std;
+#include <cstddef>
+#include "cell.h"
+#include "level.h" // forward declare?
+#include <memory>
+#include <sstream>
 
-Grid::Grid(currlevel): currlevel{currlevel} {}
+struct State;
+class TextDisplay;
+class GraphicsDisplay;
+class Block;
+class Level;
+template <typename InfoType, typename StateType> class Observer;
+class InvalidMove{};
 
-Grid::~Grid() {}
-
-void Grid::setObserver(shared_ptr<Observer<Info, State>> ob) {
-    this->ob = ob;
-}
-
-bool Grid::endGame() const {
-    throw EndException{};
-}
-
-void Grid::init() {
-    theGrid.clear();
-    liveBlocks.clear();
+class Grid {
+    std::vector<std::vector<Cell>> theGrid; //the actual grid
+    std::vector<std::shared_ptr<Block>> liveBlocks;
+    int hi_score = 100; //the highest score in game
+    int curr_score = 0; // the current score in game
+    int currlevel; //the current level
+    std::unique_ptr<std::vector<Level>> levels = nullptr; //[level0,level1,level2,...]
+    std::istringstream iss; // for block types reading if Level returns a seq file name
+    std::shared_ptr<Block> curr = nullptr; // the current block on the board
+    std::string next; // the next block will appear on the board
+    int nextlevel; // the level of next block
+    bool graphicsOn = true; // default = true, false = textonly
+    std::unique_ptr<TextDisplay> td = nullptr; // the text display
+    std::unique_ptr<GraphicsDisplay> gd = nullptr; // the graphics diaplay
     
-    curr_score = 0;
+    std::unique_ptr<Observer<Info, State>> ob = nullptr; //Another observer
+    // Add private members, if necessary.
     
-    const size_t totr = 18; // total rows
-    const size_t totc = 11; // total columns
     
-    td = new TextDisplay{};
-    if (graphicsOn) gd = new GraphicsDisplay{};
+public:
+    Grid(int currlevel);
+    ~Grid();
+    void setObserver(std::unique_ptr<Observer<Info, State>> ob);
+    bool endGame() const;  // end the game
+    void init(); // Sets up an n x n grid.  Clears old grid, if necessary.
+    void setGraphics(bool b);
+    void levelUp();
+    void levelDown();
+    int getLevel() const;
+    void setLevel(int l);
+    void clearRow(size_t r);
+    void moveDown(); // clear empty lines
+    bool isFull(size_t r);
+    bool isEmpty(size_t r); // return true if rth row is empty
+    void setCurrtoGrid();
+    void updateNext();
+    void setHint();
+    void clearHint();
+    std::string getNextType() const;
+    void setNext(std::string nextcmd); // set next, modify next level
+    void setColour(size_t row, size_t col, Colour colour);
+    void changeColour(size_t row, size_t col, Colour colour);
+    bool checkWhite(size_t row, size_t col); // return true if cell is white
+    void gSetState(size_t row, size_t col, State s) const;
+    State getState(size_t row, size_t col) const;
+    void updateScore(int point); // update curr_score & hi_score
+    int getScore() const;
+    int getHiScore() const;
+    void setLevel(size_t row, size_t col, int lev);
+    int getLevel(size_t row, size_t col);
+    Cell &getCell(size_t x, size_t y);
+    Block &currBlock();
     
-    for (size_t i = 0; i < totr; i++) {
-        vector<Cell> rArr;
-        for(size_t j = 0; j < totc; j++) {
-            Cell c{i, j, currlevel};
-            rArr.emplace_back(c);
-        }
-        theGrid.emplace_back(rArr);
-    }
-    updateNext();
-    setCurrtoGrid();
-    updateNext();
-}
+    friend std::ostream &operator<<(std::ostream &out, const Grid &g);
+};
 
-void Grid::setGraphics(bool b) {
-    graphicsOn = b;
-}
-
-void Grid::levelUp() {
-    if (currlevel < 4) ++currlevel;
-}
-
-void Grid::levelDown() {
-    if (currlevel > 0) --currlevel;
-}
-
-int Grid::getLevel() const {
-    return currlevel;
-}
-
-void Grid::setLevel(int l) {
-    currlevel = l;
-}
-
-void Grid::clearRow(size_t r) {
-    int btemp = (int)liveBlocks.size();
-    for (int i = btemp-1; i >= 0; i--) {
-        int ptemp = (int)liveBlocks[i]->getPos().size();
-        for (int j = ptemp-1; j >= 0; j--) {
-            if (liveBlocks[i]->getPos()[i].x == r) {
-                changeColour(r, liveBlocks[i]->getPos()[j].y, Colour::White);
-                liveBlocks[i]->getPos().erase(liveBlocks[i]->getPos().begin()+j);
-            }
-        }
-        if (liveBlocks[i]->getPos().size() == 0) liveBlocks.erase(liveBlocks.begin()+btemp);
-    }
-}
-
-void Grid::moveDown() {
-    size_t firstCellRow = 18;
-    for (size_t currRow= 3; currRow < 18; currRow++) {
-        if (!isEmpty(currRow) && firstCellRow == 18) firstCellRow = currRow;
-        if (isEmpty(currRow) && firstCellRow != 18) {
-            for (auto b : liveBlocks) {
-                for (auto p : b->getPos()) {
-                    if (p.x < currRow) {p.x++;}
-                }
-            }
-            firstCellRow++;
-            for (size_t rBack = currRow; rBack >= firstCellRow; rBack--) {
-                for (size_t lBack = 0; lBack < 11; lBack++) {
-                    Colour newcolour = getCell(rBack-1, lBack).getInfo().colour;
-                    getCell(rBack, lBack).changeColour(newcolour, *this);
-                    getCell(rBack, lBack).setLevel(getLevel(rBack-1, lBack), *this);
-                }
-            }
-        }
-    }
-}
-
-bool Grid::isFull(size_t r) {
-    for (size_t c = 0; c < 11; ++c) {
-        if (checkWhite(r, c)) return false;
-    }
-    return false;
-}
-
-bool Grid::isEmpty(size_t r) {
-    for (size_t c = 0; c < 11; ++c) {
-        if (!checkWhite(r, c)) return false;
-    }
-    return true;
-}
-
-void Grid::setCurrtoGrid() {
-    if (next == "I") curr = IBlock(*this, nextlevel);
-    else if (next == "J") curr = JBlock(*this, nextlevel);
-    else if (next == "L") curr = LBlock(*this, nextlevel);
-    else if (next == "S") curr = SBlock(*this, nextlevel);
-    else if (next == "Z") curr = ZBlock(*this, nextlevel);
-    else if (next == "T") curr = TBlock(*this, nextlevel);
-    else if (next == "O") curr = OBlock(*this, nextlevel);
-    else if (next == "Hint") curr = HintBlock(*this, nextlevel);
-    else if (next == "Star") curr = StarBlock(*this, nextlevel);
-}
-
-void Grid::updateNext() {
-    if (currlevel == 0) {
-        if (iss == NULL) {
-            string newLevel = levels[currlevel].createBlock();
-            iss{newLevel};
-            iss >> next;
-        }
-        else iss >> next;
-    }
-    else next = levels[currlevel].createBlock();
-    nextlevel = currlevel;
-}
-
-void Grid::setHint() {
-    HintBlock(*this);
-}
-
-void Grid::clearHint() {}
-
-string Grid::getNextType() const {
-    return next;
-}
-
-void Grid::setNext(string nextcmd) {
-    next = nextcmd;
-    nextlevel = currlevel;
-}
-
-void Grid::setColour(size_t row, size_t col, Colour colour) {
-    getCell(row, col).setColour(colour, *this);
-}
-
-void Grid::changeColour(size_t row, size_t col, Colour colour) {
-    getCell(row, col).changeColour(colour, *this);
-}
-
-bool Grid::checkWhite(size_t row, size_t col) {
-    return getCell(row, col).getInfo().colour == Colour::White;
-}
-
-void Grid::gSetState(size_t row, size_t col, State s) const {
-    getCell(row, col).setState(s);
-}
-
-State Grid::getState(size_t row, size_t col) const {
-    return getCell(row, col).getState();
-}
-
-void Grid::updateScore(int point) {
-    curr_score += point;
-    if (hi_score < curr_score) hi_score = curr_score;
-}
-
-int Grid::getScore() const {
-    return curr_score;
-}
-
-int Grid::getHiScore() const {
-    return hi_score;
-}
-
-void setLevel(size_t row, size_t col, int lev) {
-    getCell(row, col).setLevel(lev);
-}
-
-int getLevel(size_t row, size_t col) {
-    return getCell(row, col).getInfo().level;
-}
-
-Cell &Grid::getCell(size_t x, size_t y) {
-    return theGrid[x][y];
-}
-
-Block &Grid::currBlock() {
-    return curr;
-}
-
-std::ostream &operator<<(std::ostream &out, const Grid &g) {
-    out << *(g.td);
-    return out;
-}
+#endif
