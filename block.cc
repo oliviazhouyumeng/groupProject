4aed84bce38ab72173888d6e1b19b65b779d48f8
picaone <33794897+picaone@users.noreply.github.com>
2017-12-01 21:04:23 -0500

    Update block.cc

--- block.cc
+++ block.cc
@@ -14,9 +14,14 @@ void Block::cwtype() {
     else if (type == "C") type = "D";
     else if (type == "D") type = "A";
 }
+
 void Block::ccwtype() {
     if (type == "A") type = "D";
     else if (type == "B") type = "A";
     else if (type == "C") type = "B";
     else if (type == "D") type = "C";
+}
+
+vector<Posn>& Block::getPos() {
+    return pos;
 }
