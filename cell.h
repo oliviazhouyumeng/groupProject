8cc7ca134aff8f1439adbc26eb01f0966daa6074
oliviazhouyumeng <33799626+oliviazhouyumeng@users.noreply.github.com>
2017-12-02 23:34:41 -0500

    Update cell.h

--- cell.h
+++ cell.h
@@ -18,8 +18,6 @@ public:
     
     Cell(size_t r, size_t c);
     
-    void setPiece(Colour colour, Grid &g);    // Place a piece of given colour here.
-    
     void setColour(Colour colour, Grid &g); // Modifies colour of the cell, change state
     
     void changeColour(Colour colour, Grid &g); // Modifies colour of the cell, does not change state
