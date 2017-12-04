f4c6fda27426f9e48cb75b334c373dd7956611e4
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-04 02:19:42 -0500

    Update subject.h

--- subject.h
+++ subject.h
@@ -3,19 +3,6 @@
 #include <vector>
 #include <memory>
 #include "observer.h"
-/* A note on InfoType and StateType:
- 
- We have separated the collection of fields into two parts:  its Info
- and its State.
- 
- Info is for the parts of the subjject inherent to what it is:  its position
- and its colour.
- 
- State is for the parts of the subject that, when changed, trigger
- notifications.  This is the information that the Observer "gets" when it
- calls getState on the subject.  It comprises the type of state, a direction,
- and a colour.  For more details, see state.h.
- */
 
 class Grid;
 
@@ -34,8 +21,6 @@ template <typename InfoType, typename StateType>
 void Subject<InfoType, StateType>::attach(std::shared_ptr<Observer> o) {
     observers.emplace_back(o);
 }
-
-
 
 template <typename InfoType, typename StateType>
 void Subject<InfoType, StateType>::notifyObservers(Grid &g) {
