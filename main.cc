840a5a10126c5d86bb0d010cd0defef5c9a52409
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-04 01:36:39 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -14,37 +14,37 @@
 using namespace std;
 
 void Game(Grid &g, string cmd) {
-    if (cmd == "left" || cmd == "lef") {
+    if (cmd == "left" || cmd.substr(0, 3) == "lef") {
         g.currBlock().cancelHint(g);
         g.currBlock().left(g);
         cout << g << endl;
     }
-    else if (cmd == "right" || cmd == "ri" || cmd == "rig" || cmd == "righ") {
+    else if (cmd == "right" || cmd.substr(0, 2) == "ri") {
         g.currBlock().cancelHint(g);
         g.currBlock().right(g);
         cout << g << endl;
     }
-    else if (cmd == "down" || cmd == "do" || cmd == "dow") {
+    else if (cmd == "down" || cmd.substr(0, 2) == "do") {
         g.currBlock().cancelHint(g);
         g.currBlock().down(g);
         cout << g << endl;
     }
-    else if (cmd == "clockwise" || cmd == "cw") {
+    else if (cmd == "clockwise" || cmd.substr(0, 2) == "cl" || cmd == "cw") {
         g.currBlock().cancelHint(g);
         g.currBlock().clockwise(g);
         cout << g << endl;
     }
-    else if (cmd == "counterclockwise" || cmd == "ccw") {
+    else if (cmd == "counterclockwise" || cmd.substr(0, 2) == "co" || cmd == "ccw") {
         g.currBlock().cancelHint(g);
         g.currBlock().counterclockwise(g);
         cout << g << endl;
     }
-    else if (cmd == "skip" || cmd == "s") {
+    else if (cmd == "skip" || cmd.substr(0, 2) == "sk") {
         g.currBlock().cancelHint(g);
         g.updateNext();
         cout << g << endl;
     }
-    else if (cmd == "drop" || cmd == "dr") {
+    else if (cmd == "drop" || cmd.substr(0, 2) == "dr") {
         g.currBlock().cancelHint(g);
         g.currBlock().drop(g);
         try {
@@ -56,17 +56,17 @@ void Game(Grid &g, string cmd) {
             cout << "Game over!" << endl;
         }
     }
-    else if (cmd == "levelup" || cmd == "lu") {
+    else if (cmd == "levelup" || cmd.substr(0, 6) == "levelu" || cmd == "lu") {
         g.currBlock().cancelHint(g);
         g.levelUp();
         cout << g << endl;
     }
-    else if (cmd == "leveldown" || cmd == "ld") {
+    else if (cmd == "leveldown" || cmd.substr(0, 6) == "leveld" || cmd == "ld") {
         g.currBlock().cancelHint(g);
         g.levelDown();
         cout << g << endl;
     }
-    else if (cmd == "norandom") {
+    else if (cmd == "norandom" || cmd.substr(0, 1) == "n") {
         g.currBlock().cancelHint(g);//////not sure
         string seq;
         cin >> seq;
@@ -77,7 +77,7 @@ void Game(Grid &g, string cmd) {
             g.setNoRand(true);
         }
     }
-    else if (cmd == "random") {
+    else if (cmd == "random" || cmd.substr(0, 2) == "ra") {
         g.currBlock().cancelHint(g);///////not sure
         int curlev = g.getLevel();
         if (curlev >= 3) {
@@ -90,7 +90,7 @@ void Game(Grid &g, string cmd) {
         g.changeCurr(cmd);
         cout << g << endl;
     }
-    else if (cmd == "hint") {
+    else if (cmd == "hint" || cmd.substr(0, 1) == "h") {
         g.currBlock().cancelHint(g);
         g.currBlock().giveHint(g);
         cout << g << endl;
@@ -155,6 +155,7 @@ int main(int argc, char *argv[]) {
     }
     
     Grid g;
+    g.setGraphics(true);
     string cmd;
     g.setStartLevel(startLevel);
     if (textMode) g.setGraphics(false); // init a grid w/ graphics disabled
@@ -173,8 +174,10 @@ int main(int argc, char *argv[]) {
     try {
         while (true) {
             cin >> cmd;
-            if (cmd != "sequence" && cmd != "restart") game(g, cmd);
-            else if (cmd == "sequence") {
+            if (cmd != "sequence" && cmd != "restart" && 
+                cmd.substr(0, 2) != "se" && cmd.substr(0, 2) != "re") 
+                game(g, cmd);
+            else if (cmd == "sequence" || cmd.substr(0, 2) == "se") {
                 string cmdSeq;
                 cin >> cmdSeq;
                 ifstream f{cmdSeq};
@@ -182,7 +185,7 @@ int main(int argc, char *argv[]) {
                     game(g, cmd);
                 }
             }
-            else if (cmd == "restart") {
+            else if (cmd == "restart" || cmd.substr(0, 2) == "re") {
                 g.init();
                 cout << g << endl;
             }
