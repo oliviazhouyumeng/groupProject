0d3122fe02594a18f0334158341201bb74c79577
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-04 02:13:53 -0500

    Update info.h

--- info.h
+++ info.h
@@ -2,11 +2,12 @@
 #define info_h
 #include "state.h"
 #include <cstddef>
+
 enum class Colour {White=0, Black, Red, Green, Blue, Cyan, Yellow, Magenta, Orange, Brown};
+
 struct Info{
     size_t row, col;
     Colour colour;
 };
-
 
 #endif /* info_h */
