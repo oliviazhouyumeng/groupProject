ef3f30307bbfe11339fb479f2cda2fa0a6d947e1
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-12-03 20:15:05 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -9,36 +9,43 @@
 #include "block.h"
 #include "level.h"
 #include <sstream>
-#include "endexception.h"
 #include "graphicsdisplay.h"
+#include "endexception.h"
 using namespace std;
 
 void Game(Grid &g, string cmd) {
     if (cmd == "left" || cmd == "lef") {
+        g.currBlock().cancelHint(g);
         g.currBlock().left(g);
         cout << g << endl;
     }
     else if (cmd == "right" || cmd == "ri" || cmd == "rig" || cmd == "righ") {
+        g.currBlock().cancelHint(g);
         g.currBlock().right(g);
         cout << g << endl;
     }
     else if (cmd == "down" || cmd == "do" || cmd == "dow") {
+        g.currBlock().cancelHint(g);
         g.currBlock().down(g);
         cout << g << endl;
     }
     else if (cmd == "clockwise" || cmd == "cw") {
+        g.currBlock().cancelHint(g);
         g.currBlock().clockwise(g);
         cout << g << endl;
     }
     else if (cmd == "counterclockwise" || cmd == "ccw") {
+        g.currBlock().cancelHint(g);
         g.currBlock().counterclockwise(g);
         cout << g << endl;
     }
     else if (cmd == "skip" || cmd == "s") {
+        g.currBlock().cancelHint(g);
         g.updateNext();
         cout << g << endl;
     }
     else if (cmd == "drop" || cmd == "dr") {
+        g.currBlock().cancelHint(g);
         g.currBlock().drop(g);
         try {
             g.setCurrtoGrid();
@@ -50,14 +57,17 @@ void Game(Grid &g, string cmd) {
         }
     }
     else if (cmd == "levelup" || cmd == "lu") {
+        g.currBlock().cancelHint(g);
         g.levelUp();
         cout << g << endl;
     }
     else if (cmd == "leveldown" || cmd == "ld") {
+        g.currBlock().cancelHint(g);
         g.levelDown();
         cout << g << endl;
     }
     else if (cmd == "norandom") {
+        g.currBlock().cancelHint(g);//////not sure
         string seq;
         cin >> seq;
         int curlev = g.getLevel();
@@ -68,6 +78,7 @@ void Game(Grid &g, string cmd) {
         }
     }
     else if (cmd == "random") {
+        g.currBlock().cancelHint(g);///////not sure
         int curlev = g.getLevel();
         if (curlev >= 3) {
             g.setNoRand(false);
@@ -75,10 +86,15 @@ void Game(Grid &g, string cmd) {
         }
     }
     else if (cmd == "I"||cmd == "J"||cmd == "L"||cmd == "S"||cmd == "Z"||cmd == "T"||cmd == "O") {
+        g.currBlock().cancelHint(g);
         g.changeCurr(cmd);
         cout << g << endl;
     }
-    //else if (cmd == "hint")
+    else if (cmd == "hint") {
+        g.currBlock().cancelHint(g);
+        g.currBlock().giveHint(g);
+        cout << g << endl;
+    }
 }
 
 void game(Grid &g, string cmd) {
@@ -98,7 +114,7 @@ int main(int argc, char *argv[]) {
     cin.exceptions(ios::eofbit|ios::failbit);
     bool textMode = false;
     int startLevel = 0;
-    unsigned seedNum = time(NULL);
+    unsigned seedNum = time(0);
     bool setseed = false;
     string seqFile = "";
     for (int i = 1; i < argc; ++i){
@@ -141,13 +157,9 @@ int main(int argc, char *argv[]) {
     Grid g;
     string cmd;
     g.setStartLevel(startLevel);
-    if (textMode) g.Graphics(false); // init a grid w/ graphics disabled
-    if (g.isGraphics()) {
-        shared_ptr<GraphicsDisplay> GD = make_shared<GraphicsDisplay>();
-        g.setGraphics(GD);
-    }
+    if (textMode) g.setGraphics(false); // init a grid w/ graphics disabled
     if (setseed) {
-        for (auto &l : g.getLevels()) {
+        for (auto l : g.getLevels()) {
             l->setSeed(seedNum);
             l->setIsSeed(true);
         }
@@ -171,15 +183,10 @@ int main(int argc, char *argv[]) {
                 }
             }
             else if (cmd == "restart") {
-                g.setGraphics(nullptr);
-                if (g.isGraphics()) {
-                    shared_ptr<GraphicsDisplay> GD = make_shared<GraphicsDisplay>();
-                    g.setGraphics(GD);
-                }
                 g.init();
                 cout << g << endl;
-	    }
-	}
+            }
+        }
     }
     catch (ios::failure &) {}  // Any I/O failure quits
 }
