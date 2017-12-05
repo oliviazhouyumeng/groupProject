8c3262bed53fcb60b485589af73da3e48f8a4228
picaone <33794897+picaone@users.noreply.github.com>
2017-12-04 19:24:03 -0500

    Update lblock.cc

--- lblock.cc
+++ lblock.cc
@@ -235,7 +235,7 @@ void LBlock::giveHint(Grid &g) {
         }
     }
     while (true) {
-        if (br != 17 && ((g.checkWhite(br, lc) || g.checkActive(br, lc)) &&
+        if (br != 18 && ((g.checkWhite(br, lc) || g.checkActive(br, lc)) &&
                          (g.checkWhite(br, mc) || g.checkActive(br, mc)) &&
                          (g.checkWhite(br, rc) || g.checkActive(br, rc)))) {
             br++;
